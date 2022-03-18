55bcc5697041062fe194cdbffda5b2fd90b7569e
Edwinsiño Castañeda <shokatsuo@gmail.com>
2022-03-17 19:36:26 -0500

    Sepraración de header y footer
    Separé header y footer del index para que estos puedan ser usados en otros html sin necesidad de copiar todo el código

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    const ham = document.querySelector('.ham');
+    const enlaces = document.querySelector('.menu');
+    const barras = document.querySelectorAll('.ham span');
+
+
+
+    ham.addEventListener('click', () => {
+        enlaces.classList.toggle('activado');
+        barras.forEach(child => {child.classList.toggle('animado')});
+        ham.classList.toggle('girar');
+    });
+});
